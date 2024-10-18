df427d82f4086fe5f4dfbb71cf539e5b732d901d
AsdhyM <asdhyme@gmail.com>
2024-10-19 09:23:08 +1000

    feat: additional test example

--- tests/server.test.js
+++ tests/server.test.js
@@ -1,13 +1,29 @@
+// Importing the code that we want to test
 const {app} = require("../src/server");
+
+// Importing a testing helper function from supertest
 const request = require("supertest"); 
 
+// Declaring a group of tests for keeping our test tidy
 describe("Root route works", () => {
+
+    // Indivivual test happens in this type of function:
+    // This test tuns an async callback function
+    // because the things it is testing are running asynchronously and we need to wait for it
     test("Server returs a 'Hello world!' message", async () => {
         // make a request to "/" route
         // wait for response and store it
         const response = await request(app).get("/");
 
         // check the body of the response for a "Hello world!" string
+        // and any other properties that we want to check
         expect(response.body.message).toBe("Hello world!");
+        expect(response.statusCode).toBe(200);
     });
+
+    test("Server returns a response with no auth headers because we are not sending auth data", async () => {
+        const response = await(app).get("/");
+
+        expect(response.headers["Authorization"]).toBeFalsy();
+    })
 });
